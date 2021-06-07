91d8d85e168b1ced0e7ec9d25cf6b322476df776
Melia Syuhada <meliasyuhada5@gmail.com>
2021-06-08 05:49:06 +0700

    ppp

--- request_newsapp/lib/model/article_model.dart
+++ request_newsapp/lib/model/article_model.dart
@@ -11,25 +11,26 @@ class Article {
   String content;
 
   Article(
-      {this.source,
-      this.author,
-      this.title,
-      this.description,
-      this.url,
-      this.urlToImage,
-      this.publishedAt,
-      this.content});
+    {this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content});
 
-  factory Article.fromJson(Map<String, dynamic> json) {
-    return Article(
-      source: Source.fromJson(json['source']),
-      author: json['author'] as String,
-      title: json['title'] as String,
-      description: json['description'] as String,
-      url: json['url'] as String,
-      urlToImage: json['urlToImage'] as String,
-      publishedAt: json['publishedAt'] as String,
-      content: json['content'] as String,
-    );
-  }
+    factory Article.fromJson(Map<String, dynamic> json) {
+      return Article(
+        source: Source.fromJson(json['source']),
+        author: json['author'] as String,
+        title: json['title'] as String,
+        description: json['description'] as String,
+        url: json['url'] as String,
+        urlToImage: json['urlToImage'] as String,
+        content: json['content'] as String,
+
+      );
+    }
+
 }
